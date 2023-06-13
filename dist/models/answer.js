a05a59c95dd5928006b54641ad931c8777d599b7
proj-int1-ads3 <143313+denylsonmelo@users.noreply.github.com>
2023-06-13 01:09:19 +0000

    Update from https://github.com/proj-int1-ads3/huliman/commit/4a1f108b0110fc425ba489bd7c133704e18c1052

--- dist/models/answer.js
+++ dist/models/answer.js
@@ -1,1 +1,1 @@
-"use strict";(()=>{var t=class{constructor(r,e){this._id=r,this._text=e}get id(){return this._id}get text(){return this._text}};})();
+"use strict";(()=>{var e=class{constructor(t,i){this._id=t,this._text=i}get id(){return this._id}get text(){return this._text}set id(t){this._id=t}set text(t){this._text=t}};})();
